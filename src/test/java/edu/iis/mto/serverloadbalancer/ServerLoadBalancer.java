9a661136a610c6a3d25222d8cb5f364c6a8d0b1e
rafal Dzierzbinski 180157 <dzemba@op.pl>
2015-05-13 21:36:08 +0200

    test 6 refactor

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -7,17 +7,29 @@ public class ServerLoadBalancer {
 
 	public void balance(Server[] servers, Vm[] vms) {
 		for (Vm vm : vms) {
-			List<Server> serversWithEnoughCapacity = new ArrayList<Server>(servers.length);
-			for(Server server : servers){
-				if(server.canFit(vm))
-					serversWithEnoughCapacity.add(server);
-				}			
-				Server lessLoaded = extractLessLoadedServer(serversWithEnoughCapacity);
-					if(lessLoaded != null)
-						lessLoaded.addVm(vm);
+			addToCapableLessLoadedServer(servers, vm);
 		}
 
 	}
+	
+	private void addToCapableLessLoadedServer(Server[] servers, Vm vm) {
+			List<Server> serversWithEnoughCapacity = findServersWithEnoughCapacity(
+					servers, vm);
+			Server lessLoaded = extractLessLoadedServer(serversWithEnoughCapacity);
+			if (lessLoaded != null) {
+				lessLoaded.addVm(vm);
+			}
+		}
+	
+		private List<Server> findServersWithEnoughCapacity(Server[] servers, Vm vm) {
+			List<Server> serversWithEnoughCapacity = new ArrayList<Server>(
+					servers.length);
+			for (Server server : servers) {
+				if (server.canFit(vm))
+					serversWithEnoughCapacity.add(server);
+			}
+			return serversWithEnoughCapacity;
+		}
 	
 	private Server extractLessLoadedServer(List<Server> servers) {
 	Server lessLoaded = null;
