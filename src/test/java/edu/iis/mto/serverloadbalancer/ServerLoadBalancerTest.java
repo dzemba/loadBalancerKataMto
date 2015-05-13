2688b63356b108744a374251087756b708320e4f
rafal Dzierzbinski 180157 <dzemba@op.pl>
2015-05-13 21:13:23 +0200

    test 5 red

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -59,7 +59,17 @@ public class ServerLoadBalancerTest {
 		assertThat("the server should contain vm", theServer.contains(theSecondVm));
 	}
 	
-
+	@Test 
+	public void aVm_shouldBeBalanced_onLessLoadedServerFirst(){
+		Server lessLoadedServer = a(server().withCapacity(100).withCurrentLoadOf(45.0d));
+		Server moreLoadedServer = a(server().withCapacity(100).withCurrentLoadOf(50.0d));
+		Vm theVm = a(vm().ofSize(10));
+			
+		balance(aListOfServersWith(moreLoadedServer, lessLoadedServer), aListOfVmsWith(theVm));
+	
+		assertThat("the less loaded server should contain vm", lessLoadedServer.contains(theVm));
+	
+		}
 	
 			private Vm[] aListOfVmsWith(Vm... vms) {
 				return vms;
