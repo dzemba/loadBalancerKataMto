8caa6f98cf0dede942200eb86c75d24cb91addff
rafal Dzierzbinski 180157 <dzemba@op.pl>
2015-05-13 21:03:15 +0200

    test 3 refactor

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -0,0 +1,22 @@
+package edu.iis.mto.serverloadbalancer;
+
+public class Server {
+
+	private static final double MAXIMUM_LOAD = 100.0d;
+	public double currentLoadPecentage;
+	public int capacity;
+
+	public Server(int capacity) {
+		this.capacity = capacity;
+	}
+	
+	public boolean contains(Vm vm) {
+ 		return true;
+ 	}
+	
+	public void addVm(Vm vm) {
+		this.currentLoadPecentage = (double) vm.size / (double) this.capacity
+				* MAXIMUM_LOAD;
+	}
+	
+}
